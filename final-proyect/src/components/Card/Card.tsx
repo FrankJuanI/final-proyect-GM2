241a26f24246dd1d46e40002c16a936ea997aaa7
FrankJuanI <juan.frank@gm2dev.com>
2023-10-09 11:34:53 -0300

    card template

---
+++ final-proyect/src/components/Card/Card.tsx
@@ -0,0 +1,14 @@
+function Card() {
+  return (
+    <div className="card">
+      <img src="" alt="" />
+      <h3></h3>
+      <h2></h2>
+      <button></button>
+      <button></button>
+      <p></p>
+    </div>
+  );
+}
+
+export { Card };
