fbffec31425b005734ecc899549a3fd6bc33cf4e
FrankJuanI <juan.frank@gm2dev.com>
2023-10-10 11:59:31 -0300

    useDataContext function

--- final-proyect/src/App.tsx
+++ final-proyect/src/App.tsx
@@ -1,26 +1,12 @@
 import "./App.css";
 import { Home } from "./components/Home/Home";
-import { useState, useEffect } from "react";
 import { useFetch } from "./hooks/useFetch.ts";
 import { DataContext } from "./context/DataContext.ts";
-
-// interface Product {
-//   id: number;
-//   title: string;
-//   description: string;
-//   price: number;
-//   discountPercentage: number;
-//   rating: number;
-//   stock: number;
-//   brand: string;
-//   category: string;
-//   thumbnail: string;
-//   images: [];
-// }
 
 function App() {
   const { data } = useFetch();
   console.log(data);
+
   return (
     <>
       <DataContext.Provider value={data}>
