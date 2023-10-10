1f293b5a4c80f7acb812a5490ac7563d72fa9950
FrankJuanI <juan.frank@gm2dev.com>
2023-10-10 12:18:29 -0300

    feat(Products.tsx): Consume data

--- final-proyect/src/components/Products/Products.tsx
+++ final-proyect/src/components/Products/Products.tsx
@@ -1,9 +1,39 @@
 import { Card } from "../Card/Card.jsx";
+import { UseDataContext } from "../../context/DataContext.ts";
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: [];
+}
 
 export function Products() {
+  const resdata = UseDataContext();
+  console.log("data: ", resdata);
   return (
     <>
-      <Card />
+      {resdata &&
+        resdata.map((product) => {
+          return (
+            <Card
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              images={product.images}
+              description={product.description}
+              price={product.price}
+              discountPercentage={product.discountPercentage}
+            />
+          );
+        })}
     </>
   );
 }
--- final-proyect/src/context/DataContext.ts
+++ final-proyect/src/context/DataContext.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
 
-export const DataContext = createContext([]);
+export const DataContext = createContext([""]);
 
 export const UseDataContext = () => useContext(DataContext);
