dfe54096920f76dee3049e60a64f21344e1f53b7
FrankJuanI <juan.frank@gm2dev.com>
2023-10-17 11:13:06 -0300

    conditional (logged & notLoggued) nav render

--- final-proyect/src/components/IncorrectCredentials/IncorrectCredentials.tsx
+++ final-proyect/src/components/IncorrectCredentials/IncorrectCredentials.tsx
@@ -1,10 +1,10 @@
 import "./IncorrectCredentials.css";
 
 export function IncorrectCredenttials() {
-      console.log("Incorrectt credentialss")
-      return (
-        <div className="error-container">
-          <p>Incorrect Credentials</p>
-        </div>
-      );
+  console.log("Incorrectt credentialss");
+  return (
+    <div className="error-container">
+      <p>Incorrect Credentials</p>
+    </div>
+  );
 }
