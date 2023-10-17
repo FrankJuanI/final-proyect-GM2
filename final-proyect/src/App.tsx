72f1e96639209c36148b013a37309a0f8d398230
JoseeeLuis <jose.aranda@gm2dev.com>
2023-10-17 13:01:02 -0300

    chore: try the component Home

--- final-proyect/src/App.tsx
+++ final-proyect/src/App.tsx
@@ -1,23 +1,29 @@
 import "./App.css";
-import { Home } from "./components/Home/Home";
+import { Shop } from "./components/Shop/Shop.tsx";
 import { useFetch } from "./hooks/useFetch.ts";
+import { useFetchImg } from "./hooks/useBannerImg.ts";
 import { DataContext } from "./context/DataContext.ts";
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login/Login.tsx";
 import { LoginStatusProvider } from "./context/LoginStatusContext.tsx";
+import { Home } from "./components/Home/Home.tsx";
+import { ProductsDetails } from "./components/ProductDetails/ProductDetails.tsx";
 
 function App() {
   const { data } = useFetch();
   return (
     <>
-      <DataContext.Provider value={data}>
+      {/* <DataContext.Provider value={data}>
         <LoginStatusProvider>
           <Routes>
-            <Route path="home" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="shop" element={<Shop />} />
             <Route path="login" element={<Login />} />
           </Routes>
         </LoginStatusProvider>
-      </DataContext.Provider>
+      </DataContext.Provider> */}
+      {/* <ProductsDetails /> */}
+      <Home />
     </>
   );
 }
