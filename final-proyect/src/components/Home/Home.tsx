9c2ffea03acd7ad5f8273fbb2ca60fe41b65e27d
JoseeeLuis <jose.aranda@gm2dev.com>
2023-10-17 12:58:46 -0300

    feat(Home): start Home page

--- final-proyect/src/components/Home/Home.tsx
+++ final-proyect/src/components/Home/Home.tsx
@@ -1,16 +1,40 @@
-import { Nav } from "../Nav/Nav.tsx";
+import { Nav } from "../Nav/Nav";
+import { useFetchImg } from "../../hooks/useBannerImg";
 import "./Home.css";
-import { Products } from "../Products/Products.tsx";
-import { SideBar } from "../SideBar/SideBar.tsx";
+import { useEffect } from "react";
+import { useHomeData } from "../../hooks/UseHomeData";
 
-export function Home() {
+export const Home = () => {
+  const Imgs = useFetchImg();
+  const { comments, about } = useHomeData();
+
+  useEffect(() => {
+    console.log(comments, about);
+  }, [comments?.length]);
+
   return (
     <>
       <Nav />
-      <div>
-        <Products />
-        <SideBar />
+      <div className="latest-products">
+        <h2 className="home-title">Latest Products</h2>
+        <div className="Banner-img">
+          {Imgs != undefined ? <img src={`${Imgs[0]}`} alt="" /> : null}
+        </div>
+      </div>
+      <div className="about">
+        <p>About</p>
+        {about != null ? <h2>{about}</h2> : null}
+      </div>
+      <div className="comments">
+        {comments?.map((co) => {
+          return (
+            <div className="comment">
+              <p>{co.user.username}</p>
+              <p>{co.body}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
-}
+};
