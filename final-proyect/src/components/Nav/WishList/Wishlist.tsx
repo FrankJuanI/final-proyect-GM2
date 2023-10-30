6d7db0a9380f941f0c88a5eced1a24afb8ed8837
JoseeeLuis <jose.aranda@gm2dev.com>
2023-10-30 01:56:40 -0300

    fix(WishList):make wishlist responsive

--- final-proyect/src/components/Nav/WishList/Wishlist.tsx
+++ final-proyect/src/components/Nav/WishList/Wishlist.tsx
@@ -1,21 +1,26 @@
-import { useWishListContext } from "../../../context/WishListContext.tsx"
-import {WishProduct} from "./WishProduct/WishProduct.tsx"
-import "./Wishlist.css"
+import { useWishListContext } from "../../../context/WishListContext.tsx";
+import { WishProduct } from "./WishProduct/WishProduct.tsx";
+import "./Wishlist.css";
 
+export function Wishlist() {
+  const { wishlist } = useWishListContext([]);
 
-export function Wishlist (){
+  return (
+    <ul id="wishlist-ul">
+      <div className="wishlist-header">
+        <h3>Wishlist</h3>
+      </div>
 
-    const { wishlist } = useWishListContext([])
-
-
-    return (
-        <ul className="wishlist-ul">
-            <div className="wishlist-header">
-                <h3>Wishlist</h3>
+        {wishlist.length != 0 ? (
+            <div className="wishlist-menu-container">
+          {wishlist.map((product) => (
+            <WishProduct key={product.id} product={product} />
+          ))}
             </div>
-            {
-             wishlist.length != 0 ? wishlist.map((product => <WishProduct key={product.id} product={product}/>)) : <p>Not wished products</p>
-            }
-        </ul>
-    )
+        ) : (
+          <p>Not wished products</p>
+        )}
+
+    </ul>
+  );
 }
